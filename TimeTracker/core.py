c0f8c78372836b32644d6b04e90da99c0ad0d237
Hashaam Zafar <hashaam1217@gmail.com>
2024-03-03 21:19:20 -0800

    Set up grid

--- TimeTracker/core.py
+++ TimeTracker/core.py
@@ -12,18 +12,21 @@ class GCTT(Gtk.Application):
 
     def do_activate(self):
         window = Gtk.ApplicationWindow(application=self, title="Google Calendar Time Tracker")
-        button1 = Gtk.Button(label="1Click me!")
-        button2 = Gtk.Button(label="2Click me!")
 
-        def on_button_clicked1(widget):
-            print("Button clicked!1")
-        def on_button_clicked2(widget):
-            print("Button clicked!2")
+        # Create a grid container
+        grid = Gtk.Grid(column_spacing=6, row_spacing=6)
 
-        button1.connect("clicked", on_button_clicked1)
-        button2.connect("clicked", on_button_clicked2)
-        window.set_child(button1)
-        window.set_child(button2)
+        # Create buttons and add them to the grid
+        for row in range(3):
+            for col in range(2):
+                button = Gtk.Button(label=f"Button {row+1}-{col+1}")
+                button.set_hexpand(True)  # Allow horizontal expansion
+                button.set_vexpand(True)  # Allow vertical expansion
+                grid.attach(button, col, row, 1, 1)
+
+        # Set the grid as the child of the window
+        window.set_child(grid)
+
         window.present()
 
 app = GCTT()
